6efb67b28a9a3d9c60f1383b9ff54f9e732e02e9
Shahin <shahinofficial911@gmail.com>
2026-02-10 11:21:48 +0530

    update

---
+++ sample/src/features/users/usersSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchUsers = createAsyncThunk(
+  "users/fetchUsers",
+  async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    return res.json();
+  }
+);
+
+
+const usersSlice = createSlice({
+  name: "users",
+  initialState: {
+    users: [],
+    loading: false,
+    error: null
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUsers.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.loading = false;
+        state.users = action.payload;
+      })
+      .addCase(fetchUsers.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  }
+});
+
+export default usersSlice.reducer;
