36bc3ea0b759f27aa2d31eac1ddc31b304904150
Shahin <shahinofficial911@gmail.com>
2026-02-11 10:04:31 +0530

    update

---
+++ teast/src/components/Counter.jsx
@@ -0,0 +1,17 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement, reset } from '../features/counter/counterSlice'
+
+export default function Counter() {
+    const count = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch()
+    
+    return (
+        <>
+            <h1>Redux Counter</h1>
+            <h3>Count : {count}</h3>
+            <button onClick={() => dispatch(increment())}> + </button>
+            <button onClick={() => dispatch(decrement())}> - </button>
+            <button onClick={() => dispatch(reset())}> reset </button>
+        </>
+    )
+}
