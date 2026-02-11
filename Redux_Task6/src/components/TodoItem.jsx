6fda5ebb7d5a09b82a8aa17093d744ec55e1c991
Shahin <shahinofficial911@gmail.com>
2026-02-11 12:49:56 +0530

    update

---
+++ Redux_Task6/src/components/TodoItem.jsx
@@ -0,0 +1,40 @@
+import { useDispatch } from 'react-redux'
+import {
+  todoUpdated,
+  todoRemoved,
+} from '../features/todos/todosSlice'
+
+const TodoItem = ({ todo }) => {
+  const dispatch = useDispatch()
+
+  const handleToggle = () => {
+    dispatch(
+      todoUpdated({
+        id: todo.id,
+        changes: { completed: !todo.completed },
+      })
+    )
+  }
+
+  const handleDelete = () => {
+    dispatch(todoRemoved(todo.id))
+  }
+
+  return (
+    <div style={{ display: 'flex', gap: '10px', marginBottom: '10px' }}>
+      <span
+        onClick={handleToggle}
+        style={{
+          cursor: 'pointer',
+          textDecoration: todo.completed ? 'line-through' : 'none',
+        }}
+      >
+        {todo.title}
+      </span>
+
+      <button onClick={handleDelete}>Delete</button>
+    </div>
+  )
+}
+
+export default TodoItem
